3957c7db6e3938fbf496cd758ce7230ce4b3fc77
Ben Quinn <benjamesquinn@gmail.com>
2022-05-10 22:44:41 +0100

    Added patch and delete functionality

--- src/app.js
+++ src/app.js
@@ -7,16 +7,41 @@ app.use(express.json());
 
 // GET
 app.get("/cats", (req, res) => {
-  Cat.findAll(req.body).then((cat) => res.status(201).json(cat));
+  Cat.findAll(req.body, { where: req.query })
+    .then((cat) => res.status(201).json(cat))
+    .catch((error) => res.status(400).json(error));
 });
 
 app.get("/cats/:catId", (req, res) => {
-  Cat.findByPk(req.params.catId).then((cat) => res.status(201).json(cat));
+  Cat.findByPk(req.params.catId)
+    .then((cat) => res.status(201).json(cat))
+    .catch((err) => res.status(400).json(err));
 });
 
 // POST
 app.post("/cats", (req, res) => {
-  Cat.create(req.body).then((cat) => res.status(201).json(cat));
+  Cat.create(req.body)
+    .then((cat) => res.status(201).json(cat))
+    .catch((error) => res.status(400).json(error));
+});
+
+// PATCH
+app.patch("/cats/:catId", (req, res) => {
+  Cat.update(req.body, { where: { id: req.params.catId } })
+    .then((cat) => res.status(201).json(cat))
+    .catch((err) => res.status(400).json(err));
+});
+app.patch("/feed/:catId", (req, res) => {
+  Cat.update({ lastFed: new Date() }, { where: { id: req.params.catId } })
+    .then((cat) => res.status(201).json(cat))
+    .catch((err) => res.status(400).json(err));
+});
+
+// DELETD
+app.delete("/cats/:catId", (req, res) => {
+  Cat.destroy({ where: { id: req.params.catId } })
+    .then((cat) => res.status(201).json(cat))
+    .catch((err) => res.status(400).json(err));
 });
 
 module.exports = app;
